6198f90c4d55c7949bc3306514d8e4b808374f49
zkky08 <dzakyhasan8@gmail.com>
2025-05-19 15:24:09 +0700

    Menambahkan koneksi database dan tes koneksi

---
+++ src/appStokBarang/koneksi/tesKoneksi.java
@@ -0,0 +1,16 @@
+package appStokBarang.koneksi;
+
+import java.sql.Connection;
+
+public class tesKoneksi {
+
+        public static void main(String[] args) {
+            Connection conn = koneksiDB.getConnection();
+
+            if (conn != null) {
+                System.out.println("✅ Koneksi BERHASIL ke database!");
+            } else {
+                System.out.println("❌ Koneksi GAGAL!");
+            }
+        }
+}
